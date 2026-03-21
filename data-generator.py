7484cb97037cd10287842375230b51a567e16247
Faravells <139291273+Faravells@users.noreply.github.com>
2026-03-21 20:57:12 +1000

    Added connection to the database

--- data-generator.py
+++ data-generator.py
@@ -1,1 +1,10 @@
+import mysql.connector
+import os
 
+connection = None
+try:
+    connection = mysql.connector.connect(host='mysql', user='root', password=os.getenv('ROOT_PASSWORD'),
+                                         database='user_logs', port=os.getenv('DATABASE_PORT'))
+    print("Генератор данных подключен к базе данных")
+except mysql.connector.Error as e:
+    print(f"Генератору данных не удалось подключиться к базе данных: {e}")
