131b8e0c569c7cc7d93fbfc36d687173393ad3f6
Faravells <139291273+Faravells@users.noreply.github.com>
2026-03-22 22:06:01 +1000

    Added main log generation cycle

--- data-generator.py
+++ data-generator.py
@@ -2,6 +2,9 @@ import mysql.connector
 import os
 import logging
 
+def generate_logs(day):
+    return []
+
 connection = None
 try:
     connection = mysql.connector.connect(host='mysql', user='root', password=os.getenv('ROOT_PASSWORD'),
@@ -9,3 +12,16 @@ try:
     logging.error("Генератор данных подключен к базе данных")
 except mysql.connector.Error as e:
     logging.error(f"Генератору данных не удалось подключиться к базе данных: {e}")
+
+insert_query = ("""INSERT INTO log (user_id, event_id, description, forum_id, status, time)
+                VALUES (%s, %s, %s, %s, %s, %s)""")
+days = 31
+for i in range(1, days + 1):
+    logs = generate_logs(i)
+    cursor = connection.cursor()
+    for log in logs:
+        cursor.execute(query, (log['user_id'], log['event_id'], log['desc'], log['forum_id'], log['status'], log['time']))
+    connection.commit()
+    logging.error(f"Созданы логи за {i} день")
+connection.close()
+logging.error("Создание логов завершено")
