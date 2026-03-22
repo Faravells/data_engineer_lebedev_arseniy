95e7fe6aea0a849fcea058ebc04661251e4d1006
Faravells <139291273+Faravells@users.noreply.github.com>
2026-03-22 20:17:40 +1000

    Added logging to data-generator

--- data-generator.py
+++ data-generator.py
@@ -1,10 +1,11 @@
 import mysql.connector
 import os
+import logging
 
 connection = None
 try:
     connection = mysql.connector.connect(host='mysql', user='root', password=os.getenv('ROOT_PASSWORD'),
                                          database='user_logs', port=os.getenv('DATABASE_PORT'))
-    print("Генератор данных подключен к базе данных")
+    logging.error("Генератор данных подключен к базе данных")
 except mysql.connector.Error as e:
-    print(f"Генератору данных не удалось подключиться к базе данных: {e}")
+    logging.error(f"Генератору данных не удалось подключиться к базе данных: {e}")
