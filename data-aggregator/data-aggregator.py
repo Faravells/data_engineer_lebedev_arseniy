92677eb78e3eafe95a686a46a612c72815cabdc0
Faravells <139291273+Faravells@users.noreply.github.com>
2026-03-23 09:37:53 +1000

    Added database connection to data-aggregator

---
+++ data-aggregator/data-aggregator.py
@@ -0,0 +1,11 @@
+import mysql.connector
+import os
+import logging
+
+connection = None
+try:
+    connection = mysql.connector.connect(host='mysql', user='root', password=os.getenv('ROOT_PASSWORD'),
+                                         database='user_logs', port=os.getenv('DATABASE_PORT'))
+    logging.error("Агрегатор данных подключен к базе данных")
+except mysql.connector.Error as e:
+    logging.error(f"Агрегатору данных не удалось подключиться к базе данных: {e}")
